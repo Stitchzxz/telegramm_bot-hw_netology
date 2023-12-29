ad23ee1a21b2f81060a72a19e68eebc63304443a
Stitchzxz <kontsevoyn@gmail.com>
2023-12-29 09:50:09 -0300

    code_tg_bot

--- main.py
+++ main.py
@@ -1,43 +1,56 @@
+import random
 
 HELP = """
 help - напечатать справку по программе.
-add - добавить задачу в список (название задачи запрпашивается у пользователя)
-show - напечатать все добавленные задачи."""
+add - добавить задачу в список.
+show - напечатать все добавленные задачи.
+random - добавлять случайную задачу на дату "Сегодня". 
+exit - выход из программы."""
 
-today = []
-tomorrow = []
-other = []
+
+RANDOM_TASKS = ["Повысить уровень знаний", "Подготовить резюме", "Написать сопроводительное письмо", "Откликнуться на вакансию"]
+
+tasks = {
+
+}
 
 run = True
 
-while run:
+def add_todo(date, task):
+    if date in tasks:
+        # если дата есть в словаре
+        # добавить в список задачу
+        tasks[date].append(task)
+    else:
+        # если даты нет
+        # создать запись с ключом date
+        tasks[date] = []
+        tasks[date].append(task)
+    print("Задача", task, "добавлена на дату", date)
+
+while run: 
     command = input("Введите команду: ")
     if command == "help":
         print(HELP)
     elif command == "show":
-        print("Сегодня")
-        print(today)
-        print("Завтра")
-        print(tomorrow)
-        print("Другое")
-        print(other)
+        date = input("Введите дату для отображения списка задач: ")
+        if date in tasks:
+            for task in tasks[date]:
+                print('-', task)
+        else:
+            print("Такой даты нет")
     elif command == "add":
+        date = input("Введите дату для добавления задачи: ")
         task = input("Введите название задачи: ")
-        data = input("Введите дату: ")
-        if data == "сегодня":
-            today.append(task)
-        elif data == "завтра":
-            tomorrow.append(task)
-        else: 
-            other.append(task)
-        print(f"Задача {task} добавлена.")
+        add_todo(date, task)
+    elif command == "random":
+        task = random.choice(RANDOM_TASKS)
+        add_todo("Сегодня", task)
     elif command == "exit":
-        print("Спасибо за использование! До свидания!")
+        print("Спасибо за использование!")
+        break
     else:
         print("Неизвестная команда")
         break
 print("До свидания!")
 
-# print(data, today)
-# print(data, tomorrow)
-# print(data, other)fdg
